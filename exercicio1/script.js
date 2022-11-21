b1b35fbf1bf699720eb85a660bd7de91ead94bf5
Celeta <marceloceleta@gmail.com>
2022-11-21 14:12:53 -0300

    Alterações exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,31 @@
+//const nome ("")
+
+//let idade ()
+
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+//Sem valor não há identificação do tipo. Dá erro de sintaxe.
+
+const nome = prompt("Qual seu nome")
+console.log(nome)
+
+const idade = Number( prompt("Qual sua idade?"))
+console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Olá", nome, "você tem", idade, "anos")
+
+const hasPet = prompt ("Você tem pet?")
+console.log(hasPet)
+
+const filme = prompt ("Gosta de filmes?")
+console.log(filme)
+
+const gilo = prompt ("come giló?")
+console.log(gilo)
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,44 @@
+const nomeSobrenome = prompt ("Qual seu nome e sobrenome?")
+console.log(nomeSobrenome)
+
+const dataDeNascimento = prompt ("Qual sua data de nascimento? Dia/mês/ano")
+console.log(dataDeNascimento)
+
+const endereco = prompt ("Qual seu endereço?")
+console.log(endereco)
+
+const documentoCpf = prompt ("Por favor digite seu CPF.")
+console.log(documentoCpf)
+
+const escolaridade = prompt ("Qual seu nível de escolaridade?")
+console.log(escolaridade)
+
+const documentoCnh = prompt ("Possui CNH?")
+console.log(documentoCnh)
+
+const quantidadeFilhos = Number (prompt ("Quantos filhos tem?"))
+console.log(quantidadeFilhos)
+
+const cargoAtual = prompt ("Qual cargo ocupa atualmente?")
+console.log(cargoAtual)
+
+const salario = Number (prompt ("Qual o sálario atual?"))
+console.log(salario)
+
+const comissao = Number (prompt ("Recebe comissão? Se sim informe a porcentagem, caso não responda com zero."))
+console.log(comissao)
+
+const anoAdmissao = prompt("Em que ano foi admitido(a)?")
+console.log(anoAdmissao)
+
+console.log(typeof nomeSobrenome)
+console.log(typeof dataDeNascimento)
+console.log(typeof endereco)
+console.log(typeof documentoCpf)
+console.log(typeof escolaridade)
+console.log(typeof documentoCnh)
+console.log(typeof quantidadeFilhos)
+console.log(typeof cargoAtual)
+console.log(typeof salario)
+console.log(typeof comissao)
+console.log(typeof anoAdmissao)
